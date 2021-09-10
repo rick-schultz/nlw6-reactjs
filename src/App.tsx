cc98555588d723821ff66b489285fd28be27b636
Rick Schultz <rick-schultz@hotmail.com>
2021-09-10 15:41:41 -0300

    new room page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
 
 export default function App() {
   return (
-    <Home/>
+    <NewRoom/>
   );
 }
---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,32 @@
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import '../styles/auth.scss'
+import {Button} from '../components/Button'
+
+export function NewRoom() {
+ return (
+  <div id="page-auth">
+   <aside>
+    <img src={illustrationImg} alt="Illustration of Q&amp;A" />
+    <strong>Create Q&amp;A live rooms.</strong>
+    <p>Clear all of you audience doubts real-time.</p>
+   </aside>
+   <main>
+    <div className="main-content">
+     <img src={logoImg} alt="Letmeask" />
+     <h2>Create new room</h2>
+     <form>
+      <input
+      type="text"
+      placeholder="Room's name"
+      />
+      <Button type="submit">Create room</Button>
+     </form>
+     <p>
+       Wanna join an existing room? <a href="#">Click here</a>
+    </p>
+    </div>
+   </main>
+  </div>
+ )
+}
